958218b4782fda13ad075d2c0262460d435e9fd0
Blazing-Blast <lukabijma1@gmail.com>
2025-01-15 12:45:32 +0100

    Major formatting improvements

--- md-render/script.js
+++ md-render/script.js
@@ -1,6 +1,8 @@
 function renderMarkdown() {
     let filename = getArgument('filename');
-    let html = marked.render(filename);
+    let markdown = loadMarkdown(filename);
+    let converter = new Markdown.Converter();
+    let html = converter.makeHtml(markdown);
     let body = document.getElementById('MarkdownBody');
     body.innerHTML += html;
 }
@@ -12,4 +14,11 @@ function getArgument(name) {
     let param = params.find((str, idx, obj) => str.startsWith(name + '='));
     let value = param.slice(name.length + 1);
     return value;
+}
+
+function loadMarkdown(filename) {
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', filename, false); // false for synchronous request
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
